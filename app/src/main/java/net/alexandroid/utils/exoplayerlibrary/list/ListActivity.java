827b8c6b12c14a8a1462753a81e91c7e6eb55701
Pulimet <korolev2009@gmail.com>
2017-10-07 22:46:12 +0300

    Create a list for tests 2

--- app/src/main/java/net/alexandroid/utils/exoplayerlibrary/list/ListActivity.java
+++ app/src/main/java/net/alexandroid/utils/exoplayerlibrary/list/ListActivity.java
@@ -1,13 +1,12 @@
 package net.alexandroid.utils.exoplayerlibrary.list;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import net.alexandroid.utils.exoplayerlibrary.R;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -25,7 +24,12 @@ public class ListActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        ArrayList<String> list = new ArrayList<>(Arrays.asList("1", "2", "3", "4"));
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(
+                "http://ynethd-i.akamaihd.net/i/cdnwiz/1017/071017_airbos_pjAO8E9r_800.mp4/master.m3u8",
+                "http://ynethd-i.akamaihd.net/i/cdnwiz/1017/arnav_fpo8g7XF_800.mp4/master.m3u8",
+                "http://ynethd-i.akamaihd.net/i/cdnwiz/1017/031017_yadiout_sodani_subs_fix_WATXm9aJ_800.mp4/master.m3u8",
+                "http://ynethd-i.akamaihd.net/i/cdnwiz/1017/061017_2225_Harrier_performs_Reverse_Landing_TWyVXwlk_800.mp4/master.m3u8",
+                "http://ynethd-i.akamaihd.net/i/cdnwiz/0317/120317_fix_must_berlin_fix_yarden_ysIlzhaw_800.mp4/master.m3u8"));
 
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(list);
 
